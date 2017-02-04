731388af3b5522fa814b7e1cd1486d1bda479b51
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-02-04 13:31:15 +0100

    add plot of number of email sent distro

--- tools/data_exploration.py
+++ tools/data_exploration.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+
+
 def print_email_by_id(emails, mid):
     '''Uses the dataframe emails to print the email given by its mid in a
     classic format.
@@ -10,3 +13,22 @@ def print_email_by_id(emails, mid):
     email_content = "From: {sender} \nTo: {recipients}\nOn: {date} \n\
 Body:\n {body}".format(**email_dict)
     print(email_content)
+
+
+def emails_sent_distribution(email_senders, fig_number=None):
+    '''Plots the distribution of number of emails sent by sender.
+        Arguments:
+            - email_senders (pd dataframe): the pd dataframe containing the
+            emails sent for every sender.
+            - fig_number (int): the index of the plot figure.
+    '''
+    n_emails_sent = email_senders["mids"].str.split().str.len()
+    if fig_number:
+        plt.figure(fig_number)
+    else:
+        plt.figure()
+    plt.hist(n_emails_sent.as_matrix(), bins=20, normed=True)
+    plt.title("Number of email sent distribution")
+    plt.xlabel("Number of email sent")
+    plt.ylabel("Frequency")
+    plt.show()
