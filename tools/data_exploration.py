bb3c79f46817e9a8bf1388522a9d42302c178a41
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-02-05 11:42:15 +0100

    add emails received distribution

--- tools/data_exploration.py
+++ tools/data_exploration.py
@@ -1,4 +1,7 @@
+from collections import Counter
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def print_email_by_id(df_emails, mid):
@@ -19,6 +22,7 @@ def emails_sent_distribution(df_email_senders, bins=20, fig_number=None):
         Arguments:
             - df_email_senders (pd dataframe): the pd dataframe containing the
             emails sent for every sender.
+            - bins (int): the number of bins in the histogram plot.
             - fig_number (int): the index of the plot figure.
     '''
     n_emails_sent = df_email_senders["mids"].str.split().str.len()
@@ -27,7 +31,29 @@ def emails_sent_distribution(df_email_senders, bins=20, fig_number=None):
     else:
         plt.figure()
     plt.hist(n_emails_sent.as_matrix(), bins=bins, normed=True)
-    plt.title("Number of email sent distribution")
-    plt.xlabel("Number of email sent")
+    plt.title("Number of emails sent distribution")
+    plt.xlabel("Number of emails sent")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def emails_received_distribution(df_emails, bins=20, fig_number=None):
+    '''Plots the distribution of number of emails received by recipient.
+        Arguments:
+            - df_emails (pd dataframe): the pd dataframe containing the
+            emails sent for every sender.
+            - bins (int): the number of bins in the histogram plot.
+            - fig_number (int): the index of the plot figure.
+    '''
+    all_recipients = df_emails["recipients"].str.cat(sep=" ").split()
+    recipients_count = Counter(all_recipients)
+    n_emails_received = np.array(list(recipients_count.values()))
+    if fig_number:
+        plt.figure(fig_number)
+    else:
+        plt.figure()
+    plt.hist(n_emails_received, bins=bins, normed=True)
+    plt.title("Number of emails received distribution")
+    plt.xlabel("Number of emails received")
     plt.ylabel("Frequency")
     plt.show()
