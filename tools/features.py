3dbbf0a5e0e49c3b75b6cad9148fdb1cb1299966
Louis MARTIN <louisrtm@gmail.com>
2017-02-16 12:38:12 +0100

    Add split_tokenizer to features.py

--- tools/features.py
+++ tools/features.py
@@ -8,6 +8,9 @@ stemmer = LancasterStemmer()
 lemmatizer = WordNetLemmatizer()
 
 
+def split_tokenizer(s):
+    return s.split(" ")
+
 def stem(word, except_words=set()):
     if word in except_words:
         return word
