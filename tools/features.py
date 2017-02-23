b85127816e22ec7afd07f7a622043fc999d47100
Louis MARTIN <louisrtm@gmail.com>
2017-02-23 14:38:02 +0100

    Wrap graph of words in GoWVectorizer with the sklearn interface.

--- tools/features.py
+++ tools/features.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.base import BaseEstimator
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 
@@ -59,34 +60,113 @@ class Vectorizer:
         return Y
 
 
-def graph_of_words(text, window=5):
-    """ Implement the graph of word (GoW) method on the input string.
-    Args:
-        text (str): input text to be represented using GoW
-        window (int): Size of the sliding window
-    Returns:
-        words (list): list of words
-        count (list): Number of inbound edges for each word
-    """
-    graph = {}
-    # Represent a graph as a dictionary
-    # The usual implementation is:
-    #     - Each key is a node and its value is a list of its children
-    # However we will implement it in a reversed way because the final
-    # information we need is the number of inbound nodes, i.e. number
-    # of parents and not number of children. The implementation is:
-    #     - Each key is a node and its value is a list of its parents
+# Code inspired from sklearn.feature_extraction.text.CountVectorizer
+class GoWVectorizer(BaseEstimator):
+    def __init__(self, window=5):
+        self.window = 5
 
-    text = clean(text)
-    words = text.split()
-    for i, parent in enumerate(words):
-        children = words[i+1:i+window]
-        for child in children:
-            if child not in graph:
-                graph[child] = set()
-            graph[child].add(parent)
+    def _make_vocabulary(self, raw_documents):
+        """Create a vocabulary from the documents.
+        The vocabulary is a dictionary with the words as keys and their
+        associated index as value, e.g. {"foo": 0, "bar": 1, "baz": 2, ...}.
+        """
+        unique_words = sorted(set(" ".join(raw_documents).split()))
+        self.vocabulary = {word: i for i, word in enumerate(unique_words)}
+        self.n_features = len(unique_words)
+        self.feature_names = unique_words
 
-    inbound_count = {k: len(v) for k, v in graph.items()}
-    words = list(graph.keys())
-    count = [len(v) for v in graph.values()]
-    return words, count
+    def get_feature_names(self):
+        return self.feature_names
+
+    def fit(self, raw_documents, y=None):
+        """Learn a vocabulary dictionary of all tokens in the raw documents.
+        Parameters
+        ----------
+        raw_documents : iterable
+            An iterable which yields either str, unicode or file objects.
+        Returns
+        -------
+        self
+        """
+        self._make_vocabulary(raw_documents)
+        return self
+
+    def transform(self, raw_documents):
+        """Transform documents to document-term matrix.
+        Extract token counts out of raw text documents using the vocabulary
+        fitted with fit or the one provided to the constructor.
+        Parameters
+        ----------
+        raw_documents : iterable
+            An iterable which yields either str, unicode or file objects.
+        Returns
+        -------
+        X : matrix, [n_samples, n_features]
+            Document-term matrix.
+        """
+        n_samples = len(raw_documents)
+        X = np.zeros((n_samples, self.n_features))
+        for i, doc in enumerate(raw_documents):
+            X[i] = self._gow_vectorizer(doc)
+        return X
+
+    def fit_transform(self, raw_documents, y=None):
+        """Learn the vocabulary dictionary and return term-document matrix.
+        This is equivalent to fit followed by transform.
+        Parameters
+        ----------
+        raw_documents : iterable
+            An iterable which yields either str, unicode or file objects.
+        Returns
+        -------
+        X : array, [n_samples, n_features]
+            Document-term matrix.
+        """
+        self.fit(raw_documents)
+        X = self.transform(raw_documents)
+        return X
+
+    def _gow_vectorizer(self, text):
+        """ Implement the graph of word (GoW) method on the input string.
+        Args:
+            text (str): input text to be represented using GoW
+        Returns:
+            x (ndarray): vector representation of input text
+        """
+        graph = {}
+        # Represent a graph as a dictionary
+        # The usual implementation is:
+        #     - Each key is a node and its value is a list of its children
+        # However we will implement it in a reversed way because the final
+        # information we need is the number of inbound nodes, i.e. number
+        # of parents and not number of children. The implementation is:
+        #     - Each key is a node and its value is a list of its parents
+        words = text.split()
+        for i, parent in enumerate(words):
+            children = words[i+1:i+self.window]
+            for child in children:
+                if child not in graph:
+                    graph[child] = set()
+                graph[child].add(parent)
+
+        # Vector representation of input text
+        x = np.zeros(self.n_features)
+        vocabulary = self.vocabulary
+        for word, parents in graph.items():
+            if word in vocabulary:
+                index = vocabulary[word]
+                inbound_edges = len(parents)
+                # We normalize with window to have lower values
+                # I don't know if it improves performance but it seemed like a
+                # good idea.
+                x[index] = inbound_edges/self.window
+        return x
+
+    def most_important_words(self, text):
+        """ Returns a comprehensive list of tuples (word, count) sorted
+        in decreasing order of importance.
+        The count is the graph of words measure of number of inbound edges."""
+        counts = self._graph_of_words(text)
+        words = self.get_feature_names()
+        results = [(x, y) for (y, x) in sorted(zip(counts, words))][::-1]
+        return results
