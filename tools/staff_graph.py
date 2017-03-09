bed377615d1113553e0e69da70829b59e3e3c26d
pdubreuil1 <pierre.dubreuil@telecom-paristech.fr>
2017-03-09 11:28:39 +0100

    changed the PR

--- tools/staff_graph.py
+++ tools/staff_graph.py
@@ -2,13 +2,14 @@ import community
 import networkx as nx
 import numpy as np
 import pandas as pd
+
 from matplotlib import pylab as pl
 
 
 def construct_graph(df_emails):
     '''From an email dataframe, output the biggest connected component of the
         graph where nodes are sender/recipients and edges are emails sent
-        between them
+        between them.
         Arguments:
             - df_emails (pd dataframe): the pd dataframe containing the emails
         Ourput:
@@ -17,17 +18,16 @@ def construct_graph(df_emails):
     G = nx.Graph()
     for index, row in df_emails.iterrows():
         sender = row["sender"]
-        G.add_node("sender")
+        G.add_node(sender)
         recipients = row["recipients"]
         recipients_list = recipients.split()
         for rec in recipients_list:
             if "@" in rec:
-                G.add_node(str(rec))
-                G.add_edge(sender, str(rec), weight=1/len(recipients_list))
+                G.add_node(rec)
+                G.add_edge(sender, rec, weight=1/len(recipients_list))
 
-    if print(nx.is_connected(G)) is False:
-        list_graph = list(nx.connected_component_subgraphs(G))
-        G_connected = list_graph[0]
+    if not nx.is_connected(G):
+        G_connected = next(nx.connected_component_subgraphs(G))
     else:
         G_connected = G
     return G_connected
@@ -43,28 +43,25 @@ def compute_teams(G):
                 - n_classes (int): number of clusters
     '''
     parts = community.best_partition(G)
-    values = {}
+    teams = {}
     for node in G.nodes():
-        values[node] = parts.get(node)
-    counter_set = set()
-    for x in values:
-        output.add(counter_set)
-    n_clusters = len(counter_set)
-    dict = {}
-    dict["n_clusters"] = n_clusters
-    dict["values"] = values
-    return dict
+        teams[node] = parts.get(node)
+    n_clusters = len(set(teams.values()))
+    output_dict = {}
+    output_dict["n_clusters"] = n_clusters
+    output_dict["values"] = values
+    return output_dict
 
 
-def assign_team(values, n_clusters, email):
+def assign_team(teams, n_clusters, email):
     '''Construct a vector to figure out in which team 'email' is.
         Arguments:
-            - values: assignmenet from Louvain algo.
+            - teams: assignmenet from Louvain algo.
             - n_clusters: number of clusters in the Louvain assignment
             - email (str): email address (one of the nodes of G)
         Ourput:
             - assignment (np.array): team assigned.
     '''
     assignment = np.zeros(n_clusters)
-    assignment[values[email]] = 1
+    assignment[teams[email]] = 1
     return assignment
