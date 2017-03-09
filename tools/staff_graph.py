4106e88bb7849f4126c14721792b71ab3ad96638
pdubreuil1 <pierre.dubreuil@telecom-paristech.fr>
2017-03-09 12:44:18 +0100

    adding a compute_summary_graph function

--- tools/staff_graph.py
+++ tools/staff_graph.py
@@ -50,6 +50,7 @@ def compute_teams(G):
     output_dict = {}
     output_dict["n_clusters"] = n_clusters
     output_dict["teams"] = teams
+    output_dict["parts"] = parts
     return output_dict
 
 
@@ -65,3 +66,37 @@ def assign_team(teams, n_clusters, email):
     assignment = np.zeros(n_clusters)
     assignment[teams[email]] = 1
     return assignment
+
+
+def compute_summary_graph(G, n_clusters, parts):
+    '''Construct a summary graph to see the weights between classes and internal
+        Arguments:
+            - G: graph where each node is assigned to a team in 'parts'.
+            - n_clusters: number of clusters in the Louvain assignment
+        Ourput:
+            - G_community (nx graph): team assigned.
+    '''
+    G_community = nx.Graph()
+    for node in G.nodes():
+        value = parts.get(node)
+        G_community.add_node(value)
+    for source_email, sink_email in G.edges():
+        source_value = parts.get(source_email)
+        sink_value = parts.get(sink_email)
+        if G_community.has_edge(source_value, sink_value):
+            # we added this one before, just increase the weight by one
+            G_community[source_value][sink_value]['weight'] += G.get_edge_data(
+                source_email,
+                sink_email
+                )['weight']
+        else:
+            # new edge. add with weight=weight in G
+            G_community.add_edge(
+                source_value,
+                sink_value,
+                weight=G.get_edge_data(source_email, sink_email)['weight'])
+    # renaming nodes with their intern weight
+    relabeling_dict = {}
+    for node in G_community.nodes():
+        relabeling_dict[node] = int(G_community[node][node]['weight'])
+    G_community = nx.relabel_nodes(G_community, relabeling_dict)
